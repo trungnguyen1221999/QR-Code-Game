2fd6120edc9f8207144c729d555e1ccf34650078
shunlei99 <shun.oo@vuoksi.fi>
2026-03-26 10:30:44 +0200

    Update checkpointRewards.js

--- frontend/src/utils/checkpointRewards.js
+++ frontend/src/utils/checkpointRewards.js
@@ -1,8 +1,8 @@
 export const CHECKPOINT_SECRET_REWARDS = {
-  1: { name: 'Hat', image: '/storyItem/sacredhat(memory).png' },
+  1: { name: 'Totem', image: '/storyItem/sacredtotem(tower).png' },
   2: { name: 'Hammer', image: '/storyItem/sacredhammer(whakAMole).png' },
   3: { name: 'Scroll', image: '/storyItem/sacredscroll(quiz).png' },
-  4: { name: 'Totem', image: '/storyItem/sacredtotem(tower).png' },
+  4: { name: 'Hat', image: '/storyItem/sacredhat(memory).png' },
   5: { name: 'Stone', image: '/storyItem/sacredstone(puzzle).png' },
   6: { name: 'Necklace', image: '/storyItem/sacrednecklace(simon).png' },
 };
@@ -10,3 +10,6 @@ export const CHECKPOINT_SECRET_REWARDS = {
 export function getCheckpointSecretReward(checkpoint) {
   return CHECKPOINT_SECRET_REWARDS[checkpoint] || CHECKPOINT_SECRET_REWARDS[1];
 }
+
+
+
